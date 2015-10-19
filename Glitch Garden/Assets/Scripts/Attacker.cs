b64a1f0a0d97e9267a3ac45137d0dda53c9fe76b
Rhys <rhysdavies1@gmail.com>
2015-10-19 22:50:45 +1030

    Redid animations on parent :(
    Added in animation function calls

---
+++ Glitch Garden/Assets/Scripts/Attacker.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Attacker : MonoBehaviour {
+
+	[Range (-1f, 1.5f)]	
+	public float currentSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.Translate (Vector3.left * currentSpeed * Time.deltaTime);
+	
+	}
+
+	void OnTriggerEnter2D(){
+
+
+
+	}
+
+	public void SetSpeed (float speed){
+
+		currentSpeed = speed;
+
+	}
+
+	public void StrikeCurrentTarget(float damage){
+
+		Debug.Log ("Dealt " + damage + " damage!");
+
+	}
+
+}
